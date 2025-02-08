5b5fb63ddd57f63cfac6fa44a1fff591aeb353b1
WXYiiiii <48340613+WXYiiiii@users.noreply.github.com>
2025-02-08 10:00:20 +0800

    20250208DSP代码

--- 02_py_etl功能代码/demo/tmp.py
+++
@@ -1,3 +0,0 @@
-hex_str = "48656c6c6f"  # 示例十六进制字符串
-byte_data = bytes.fromhex(hex_str)
-print(byte_data)  # 输出: b'Hello'
--- py积累/241016-读取csv文件.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-# 读取CSV文件并将每一行存储为列表
-with open('data.csv', encoding='utf-8') as csvfile:
-    csv_reader = csv.reader(csvfile)
-    data_list = [row for row in csv_reader]
-
-print(data_list)
